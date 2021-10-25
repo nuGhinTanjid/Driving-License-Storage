83dd6bb1a9adf7717e2da611bf2a96f842d6c81c
nuGhinTanjid <tanjidit2@gmail.com>
2021-10-26 00:00:28 +0600

    Allah hu Akbar 0

---
+++ src/drivingLicenseTracker/Main.java
@@ -0,0 +1,23 @@
+package drivingLicenseTracker;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("DrivingLicenseInformationTracker.fxml"));
+        primaryStage.setTitle("Driving License Information Tracker");
+        primaryStage.setScene(new Scene(root));
+        primaryStage.sizeToScene();
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
